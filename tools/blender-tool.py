6180e4fbf9a4f5d99910f178d2c2cee1b71d2504
Frederic Delaunay <frederic.delaunay@manymakers.net>
2023-08-02 23:14:36 +0200

    MOD: companion script auto-detection

--- tools/blender-tool.py
+++ tools/blender-tool.py
@@ -48,28 +48,53 @@ def new_FileBrowserClass(
     return Any_OT_FileBrowser
 
 
-def show_msg(msg, title='Sapien Mat importer', icon='INFO'):
+def console_get():
+    for area in bpy.context.screen.areas:
+        if area.type == 'CONSOLE':
+            for space in area.spaces:
+                if space.type == 'CONSOLE':
+                    return area, space
+    return None, None
+
+def console_write(text):
+    area, space = console_get()
+    if space is None:
+        return
+
+    context = bpy.context.copy()
+    context.update(dict(
+        space=space,
+        area=area,
+    ))
+    for line in text.split("\n"):
+        bpy.ops.console.scrollback_append(context, text=line, type='OUTPUT')
+
+
+def show_msg(msg, title='Sapien Mat importer', icon='INFO', also_log=True):
     bpy.context.window_manager.popup_menu(lambda s, c:
         s.layout.label(text=msg),
         title=title, 
         icon=icon
     )
+    also_log and console_write(msg)
 
-def import_util(fb_operator):
+
+def load_module(path : Path):
     global mats_importer
     try:
-        bpy.ops.sapiens.open_filebrowsermaterial('INVOKE_DEFAULT')
-
         ## import an unknown module name 
         from importlib import util
         spec = util.spec_from_file_location(
-            'sapiens_modding.importer',
-            fb_operator.filepath
-        )
+            'sapiens_modding.importer', path )
         mats_importer = util.module_from_spec(spec)
         spec.loader.exec_module(mats_importer)
     except ImportError as e:
         return f"could not import {self.filepath} : {e}"
+
+
+def import_util(fb_operator):
+    bpy.ops.sapiens.open_filebrowsermaterial('INVOKE_DEFAULT')
+    return load_module(fb_operator.filepath)
 
 def import_mats(fb_operator):
     global mats_importer
@@ -80,23 +105,33 @@ def import_mats(fb_operator):
     bMat_type = type(bpy.data.materials.new('delete_me'))
 
     ## now create the mats
+    result = {'OK': 0, 'KO': 0, 'NOP': 0}
     for mat_name, mat_def in materials.items():
         if mat_name in bpy.data.materials:
             if not fb_operator.override:
+                result['NOP'] += 1
                 continue
             else:
                 mat = materials[mat_name]
         else:
             mat = bpy.data.materials.new(mat_name)
         # edit material
+        edited = False
         if hasattr(mat, 'diffuse_color')    and 'color' in mat_def:
             for i, c in enumerate(mat_def['color']):
                 mat.diffuse_color[i] = c
+                edited = True
         if hasattr(mat, 'metallic')         and 'metal' in mat_def:
             mat.metallic = mat_def['metal']
+            edited = True
         if hasattr(mat, 'roughness')        and 'roughness' in mat_def:
             mat.roughness = mat_def['roughness']
-        
+            edited = True
+        result['OK' if edited else 'KO'] += 1
+        if not edited:
+            console_write(f"\tmat: {dir(mat)}\n\tmat_def: {dir(mat_def)}")
+    show_msg(f"import done. Result: {result['OK']} imported, {result['KO']} failed, {result['NOP']} ignored")
+
 
 ## Create 2 FileBrowser operators to locate our files   #TDL: find a more elegant way to do this?
 Sapiens_OT_Open_FilebrowserImport = new_FileBrowserClass(
@@ -114,10 +149,18 @@ Sapiens_OT_Open_FilebrowserMaterial = new_FileBrowserClass(
 )
 
 if __name__ == "__main__":
-    # register for one-shot
-    bpy.utils.register_class(Sapiens_OT_Open_FilebrowserImport)
+    ## file browser for material.lua is used unconditionally
     bpy.utils.register_class(Sapiens_OT_Open_FilebrowserMaterial)
 
-    ## open python script, then material.lua
-    bpy.ops.sapiens.open_filebrowserimport('INVOKE_DEFAULT')
+    path_this_script = Path(bpy.context.space_data.text.filepath)
+    path_util_script = path_this_script.with_name('material_lib_importer.py')
+    if path_util_script.exists():
+        load_module(path_util_script)
+        ## open material.lua and import materials
+        bpy.ops.sapiens.open_filebrowsermaterial('INVOKE_DEFAULT')
+    else:
+        # register for one-shot
+        bpy.utils.register_class(Sapiens_OT_Open_FilebrowserImport)
+        ## open python script, then material.lua and import materials
+        bpy.ops.sapiens.open_filebrowserimport('INVOKE_DEFAULT')
 
